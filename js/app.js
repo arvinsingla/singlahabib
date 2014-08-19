295f437afa1a7dd2bc58e184691bcdf464dba8bb
Arvin Singla <arvin.singla@gmail.com>
2014-08-18 22:52:22 -0400

    Primary styling for site, added most images and icons.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,62 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+// Attach fastclick to the body
+FastClick.attach(document.body);
+
+// jQuery specific functionality for the front end.
+(function ($) {
+
+  /**
+   * Cats Section
+   */
+
+  // Cats peeking in/out of the viewport.
+  var initializeCats = function() {
+    $('.cat')
+    .waypoint(function(direction) {
+      switch(direction) {
+        case 'down':
+          $(this).parent().addClass('peek');
+          break;
+        case 'up':
+          $(this).parent().removeClass('peek expand');
+          break;
+      }
+    }, { offset: '80%' })
+    .waypoint(function(direction) {
+      switch(direction) {
+        case 'down':
+          $(this).parent().removeClass('peek expand');
+          break;
+        case 'up':
+          $(this).parent().addClass('peek');
+          break;
+      }
+    }, { offset: '10%' })
+    // Click handler to expand peeking cat.
+    .click(function(e) {
+      e.preventDefault();
+      $(this).parent().toggleClass('expand');
+    });
+  };
+
+  /**
+   * Page Loading Events
+   */
+  // Events to fire when the page has fully loaded.
+  $(window).bind("load", function() {
+    // Add the loaded class when the page has fully loaded.
+    $('body').addClass('loaded');
+    // Curve the main intro text on an arc
+    $("h1.intro-title").arctext({radius: 2500});
+    // Initialize the cats
+    initializeCats();
+    // Initialize the slider
+    $('.flexslider').flexslider({
+      animation: "slide"
+    });
+  });
+
+}(jQuery));
